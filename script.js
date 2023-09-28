71c611175fbb00c1913cd4f2045fb43b4d6ddf9e
Memouna Irain <memounairain@Memounas-MacBook-Pro.local>
2023-09-28 14:37:13 +0100

    filter is working

--- script.js
+++ script.js
@@ -207,81 +207,74 @@ $(document).ready(function() {
     displayWatchlist();
   }
 
+  // js code for filters -- not working yet
+  // Added event listeners to filter options to trigger filtering
+
+
+  $('#MovieYear a').click(function () {
+
+    $(this).toggleClass('active');
+    console.log('Year filter clicked.'); 
+    // getting value 
+    const selectedYear = $('.li-year .dropdown-item a.active').text();
+    //clear the selected value
+    $('.li-year .dropdown-item a.active').removeClass('active');
+    // condition check on empty value
+    !selectedYear ? console.log("Year is empty.") : filterMovies(selectedYear);
+
+  });
+
+  // Function to filter and display movies based on selected criteria
+  function filterMovies(year) { 
+
+// Making API request
+const apiKey = 'c6ff91ff';
+let apiUrl = `https://www.omdbapi.com/?s=movie&type=movie&apikey=${apiKey}&`;
+
+  if (year && year !== 'All') {
+    apiUrl += `y=${year}&`;
+    }
+
+$.ajax({
+  url: apiUrl,
+  method: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    displayFilteredMovies(data.Search);
+  },
+  error: function (error) {
+    console.error('Error fetching filtered data:', error);
+  },
+});
+   
+  }
+
+  // Function to display filtered movies in placeholders
+  function displayFilteredMovies(movies) {
+
+    console.log('Displaying filtered movies:', movies); 
+
+    // Clear existing movie placeholders
+   // $('.col-md-3 .card').empty();
+
+    // Loop through the filtered movies and populate placeholders
+    for (let i = 0; i < movies.length; i++) {
+      const movie = movies[i];
+      console.log(movie);
+      const card = $('.col-md-3 .card').eq(i); 
+      console.log(card);
+            // Set the movie title
+      card.find('.card-text').text(movie.Title);
+
+      // Set the movie poster
+      const img = card.find('.card-img-top');
+      img.attr('src', movie.Poster);
+      img.attr('alt', movie.Title);
+    }
+  }
+
+  // Initial filtering when the page loads
+  filterMovies();
+
   fetchMovieData();
 });
-
-//js code for filters -- not working yet
-// Add event listeners to filter options to trigger filtering
-$('#MovieGenre a').click(function () {
-  $(this).toggleClass('active');
-  filterMovies();
-});
-
-$('#MovieYear a').click(function () {
-  $(this).toggleClass('active');
-  filterMovies();
-});
-
-$('#MovieAge-rating a').click(function () {
-  $(this).toggleClass('active');
-  filterMovies();
-});
-
-// Function to filter and display movies based on selected criteria
-function filterMovies() {
-  // Get selected genre, year, and age rating
-  const selectedGenre = $('#MovieGenre a.active').text();
-  const selectedYear = $('#MovieYear a.active').text();
-  const selectedAgeRating = $('#MovieAge-rating a.active').text();
-
-  // Making API request
-  const apiKey = 'c6ff91ff';
-  let apiUrl = `https://www.omdbapi.com/?s=movie&type=movie&apikey=${apiKey}&`;
-
-  // Construct the API URL with selected filters
-  if (selectedGenre !== 'All') {
-    apiUrl += `genre=${selectedGenre}&`;
-  }
-  if (selectedYear !== 'All') {
-    apiUrl += `y=${selectedYear}&`;
-  }
-  if (selectedAgeRating !== 'All') {
-    apiUrl += `rating=${selectedAgeRating}&`;
-  }
-
-  $.ajax({
-    url: apiUrl,
-    method: 'GET',
-    dataType: 'json',
-    success: function (data) {
-      displayFilteredMovies(data.Search);
-    },
-    error: function (error) {
-      console.error('Error fetching filtered data:', error);
-    },
-  });
-}
-
-// Function to display filtered movies in placeholders
-function displayFilteredMovies(movies) {
-  // Clear existing movie placeholders
-  $('.col .card').empty();
-
-  // Loop through the filtered movies and populate placeholders
-  for (let i = 0; i < movies.length; i++) {
-    const movie = movies[i];
-    const card = $('.col .card').eq(i);
-
-    // Set the movie title
-    card.find('.card-text').text(movie.Title);
-
-    // Set the movie poster
-    const img = card.find('.card-img-top');
-    img.attr('src', movie.Poster);
-    img.attr('alt', movie.Title);
-  }
-}
-
-// Initial filtering when the page loads
-filterMovies();
-
